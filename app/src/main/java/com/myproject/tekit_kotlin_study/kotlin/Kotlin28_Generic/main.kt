da493d3c7badcd2b3d7db29cf29c1b6f99c3d55f
chanho <uni070@naver.com>
2024-01-01 09:50:16 +0900

    2024/01

--- app/src/main/java/com/myproject/tekit_kotlin_study/kotlin/Kotlin28_Generic/main.kt
+++ app/src/main/java/com/myproject/tekit_kotlin_study/kotlin/Kotlin28_Generic/main.kt
@@ -3,7 +3,13 @@ package com.myproject.tekit_kotlin_study.kotlin.Kotlin28_Generic
 // 결정할 수 있도록 하는 개념
 
 fun main() {
+    // 제네릭 타입은 객체를 생성할 때 타입을 결정해줘야 한다.
+    // T 타입으로 정의된 변수의 타입이 Int 로 결정된다.
+    val t1 = TestClass1<Int>()
+    t1.testMethod1(100)
 
+    val t2 = TestClass1<String>()
+    t2.testMethod1("안녕하세요")
 }
 
 // < > : 안에 알바벳 문자를 넣어준다. 보통 대문자 한글자를 작성한다.
