e763fb70a1e6b125277bb2e22a467cfb0031aa7f
chanho <uni070@naver.com>
2024-01-01 10:16:12 +0900

    2024/01

--- app/src/main/java/com/myproject/tekit_kotlin_study/kotlin/Kotlin28_Generic/main.kt
+++ app/src/main/java/com/myproject/tekit_kotlin_study/kotlin/Kotlin28_Generic/main.kt
@@ -10,6 +10,20 @@ fun main() {
 
     val t2 = TestClass1<String>()
     t2.testMethod1("안녕하세요")
+
+    val t3 = TestClass2<Int, Double, Boolean, String>(100, 11.11)
+    t3.testMethod3(true, "문자열1")
+
+    val t4 = TestClass2<Double, String, Boolean, Int>(22.22, "문자열2")
+    t4.testMethod3(false, 200)
+
+    // 불변성 : 객체를 생성할 때 설정한 제네릭과 같은 변수에 담을 수 있다.
+    // 클래스간의 상속 관계에 상관없이 제네릭에 설정한 클래스 타입이 다르면 오류가 발생한다.
+    val t5:TestClass3<SubClass1> = TestClass3<SubClass1>()
+    //val t6:TestClass1<SuperClass1> = TestClass3<SubClass1>()
+    //val t7:TestClass1<SuperClass1> = TestClass3<SubClass1>()
+    val t8: TestClass4<SubClass1> = TestClass4<SubClass1>()
+    val t9: TestClass4<SubClass1> = TestClass4<SubClass1>()
 }
 
 // < > : 안에 알바벳 문자를 넣어준다. 보통 대문자 한글자를 작성한다.
@@ -21,3 +35,26 @@ class TestClass1<T>{
         println("a1 : $a1")
     }
 }
+
+class TestClass2<A, B, C, D>(var a1:A, var a2:B){
+
+    fun testMethod3(a3:C, a4:D){
+        println("a1 : $a1")
+        println("a2 : $a2")
+        println("a3 : $a3")
+        println("a4 : $a4")
+    }
+}
+
+open class SuperClass1
+open class SubClass1 : SuperClass1()
+class SubClass2 : SubClass1()
+
+// 불변성 (제네릭에 키워드를 붙히지 않는다)
+class TestClass3<T>
+
+// 공변성
+class TestClass4<out A>()
+
+// 반 공변성
+class TestClass5<in A>()
