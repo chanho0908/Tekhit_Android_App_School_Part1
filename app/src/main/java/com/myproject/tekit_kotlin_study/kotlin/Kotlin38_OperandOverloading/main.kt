06f2b92736b240abde65becf0fad3fd158c7089d
chanho <uni070@naver.com>
2024-01-09 11:30:16 +0900

    2024/01/08

---
+++ app/src/main/java/com/myproject/tekit_kotlin_study/kotlin/Kotlin38_OperandOverloading/main.kt
@@ -0,0 +1,11 @@
+package com.myproject.tekit_kotlin_study.kotlin.Kotlin38_OperandOverloading
+
+fun main(){
+    val obj1 = TestClass1(100, 200)
+    val obj2 = TestClass1(10, 20)
+}
+
+class TestClass1(var a1:Int, var a2:Int){
+
+}
+
